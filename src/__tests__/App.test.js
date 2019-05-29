a60bc43f7fc74b42089d1416844d8233c8e780d2
3ddysan <3ddysan@users.noreply.github.com>
2019-05-29 22:29:29 +0200

    test: Add simple timer acceptance test

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'App';
+import { render, fireEvent, act, cleanup } from 'react-testing-library';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+jest.useFakeTimers();
+
+describe('App', () => {
+
+  afterEach(cleanup);
+
+  it('timer after 10 seconds', () => {
+    const { getByTestId } = render(<App />)
+
+    const startButton = getByTestId("startButton");
+
+    act(() => {
+      fireEvent.click(startButton);
+      jest.advanceTimersByTime(10000);
+    });
+
+    const display = getByTestId("display");
+    expect(display.textContent).toBe('00Hours00Minutes10Seconds')
+  });
+
 });
