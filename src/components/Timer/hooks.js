f0667eff92dd280b5ce105bc9f25b7ab4483bba5
3ddysan <3ddysan@users.noreply.github.com>
2019-06-01 23:55:05 +0200

    ref: remember last callback for interval (useRef)

--- src/components/Timer/hooks.js
+++ src/components/Timer/hooks.js
@@ -1,15 +1,20 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { transformSeconds } from 'helper'
 
 export const useInterval = (callback, active = false) => {
     const [isActive, setActive] = useState(active)
     const state = isActive ? 'running' : 'stopped'
+    const callbackRef = useRef();
+
+    useEffect(() => {
+        callbackRef.current = callback;
+    }, [callback]);
 
     useEffect(() => {
         if (isActive) {
             const id = setInterval(() => {
                 if (state === 'running') {
-                    callback()
+                    callbackRef.current()
                 }
             }, 1000);
             return () => clearInterval(id)
@@ -28,7 +33,7 @@ export const useTimer = (active = false, elapsedSeconds = 0) => {
     const enhancedState = (!isActive && seconds > 0 ? 'paused' : state);
 
     const toggle = (fn) => {
-        setActive(isActive =>  {
+        setActive(isActive => {
             const newState = !isActive;
             fn && fn(newState)
             return newState
