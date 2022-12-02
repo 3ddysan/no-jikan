08159605692b15b4f084466a79eb4b7b512f7a14
3ddysan <3ddysan@users.noreply.github.com>
2022-12-02 20:47:48 +0100

    build(vite): add helper plugins

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,3 @@
-<script setup lang="ts">
-import Logo from '@/Logo.vue';
-</script>
-
 <template>
   <Logo />
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({ dts: 'src/components.d.ts' }),
+    AutoImport({
+      dts: 'src/auto-imports.d.ts',
+      imports: ['vue', '@vueuse/core'],
+      eslintrc: {
+        enabled: true,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
